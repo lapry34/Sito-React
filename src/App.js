7dd02d50da8281d8adc24f6a874aabd736aa0ccd
Gabriele Onorato <lapry34@gmail.com>
2020-09-12 14:04:11 +0200

    Aggiunta progetti

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
-import foto_esempio from './media/img/profilo.png';
+import foto from './media/img/profilo.png';
 import lazio_innova from './media/img/lazio.png';
-import trashbot from './media/img/trashbot.png'
+import trashbot from './media/img/trashbot.png';
+import drone from './media/img/drone.png';
+import zr from './media/img/zr.png';
 
 const name = "Gabriele Onorato";
-const profession = "Software Engineer";
+const profession = "Software Developer";
 
 
 class Index extends React.Component {
@@ -28,7 +30,7 @@ class Index extends React.Component {
                     <tr>
                         <td>
                             <div className="center">                   
-                                <Button onClick={btnContatti} icona="fas fa-address-book"  />
+                                <Button onClick={btnContatti} icona="fas fa-address-book" style={{marginRight: "3em"}} />
                                 <Button onClick={btnProgetti} icona="fas fa-wrench"  />
                             </div>
                         </td>
@@ -58,9 +60,9 @@ class Contatti extends React.Component {
                         <tr>
                             <td>
                                 <div className="center"> 
-                                    <Button redirect="https://resume.io/r/vhINdooY5" icona="fas fa-address-card" />
-                                    <Button redirect="https://www.linkedin.com/in/gabriele-onorato-256924199/" icona="fab fa-linkedin" />
-                                    <Button redirect="https://github.com/lapry34" icona="fab fa-github-square" />
+                                    <Button href="https://resume.io/r/vhINdooY5" icona="fas fa-address-card" />
+                                    <Button href="https://www.linkedin.com/in/gabriele-onorato-256924199/" icona="fab fa-linkedin" />
+                                    <Button href="https://github.com/lapry34" icona="fab fa-github-square" />
                                 </div>
                             </td>
                         </tr>
@@ -89,7 +91,9 @@ class Progetti extends React.Component {
                         <td>
                             <div className="center">
                                 <ProjCard img={lazio_innova} title="Lazio Innova" desc="Regione Lazio" period="2018-2019" href="http://www.lazioinnova.it/startupper-school-academy/startupper-banchi-scuola/" />
-                                <ProjCard img={trashbot} title="TrashBot" desc="ITIS E. Fermi" period="2019-Now" href="https://trashbot.onecleaner.it" />
+                                <ProjCard img={zr} title="ZeroRobotics" desc="MIT Programming Challenge" period="2018-2019" href="http://zerorobotics.mit.edu" />
+                                <ProjCard img={trashbot} title="TrashBot" desc="ITIS E. Fermi" period="2019-2020" href="https://trashbot.it" />
+                                <ProjCard img={drone} title="AidDrone" desc="Air Drone Service" period="2019-Now" href="#" />
                             </div>
                         </td>
                     </tr>
@@ -106,7 +110,7 @@ class Button extends React.Component {
     render() {
 
         return (
-            <a href={this.props.redirect}><button className="white-button" onClick={this.props.onClick}><i className={this.props.icona}></i></button></a>
+            <a href={this.props.href}><button className="white-button" onClick={this.props.onClick} style={this.props.style}><i className={this.props.icona}></i></button></a>
         );
     }
 
@@ -130,7 +134,7 @@ class ProfileImg extends React.Component {
         return (
 
             <div id="profile-image-div">
-            <img id="profile-image" src={foto_esempio} alt="..." />            
+            <img id="profile-image" src={foto} alt="..." />            
             </div>
         );
     }
